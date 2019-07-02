185d786678e646f2380a032ccf2e2f29c3a0f743
Xavier Olivares <xm.olivares@gmail.com>
2019-07-02 14:52:33 -0400

    FIX(jobBoard): cleaned up non-useful code

--- src/component/JobBoard.js
+++ src/component/JobBoard.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import axios from "axios";
 
 const jobs = require("../jobs");
 
@@ -9,24 +8,6 @@ export default class JobBoard extends Component {
     const showHideClassName = show
       ? "modal display-block"
       : "modal display-none";
-  }
-
-  componentDidMount() {
-    const { data } = axios.get("https://jobs.github.com/positions.json");
-    console.log("DATA:", data);
-  }
-
-  createCORSRequest(method, url) {
-    var xhr = new XMLHttpRequest();
-    if ("withCredentials" in xhr) {
-      // Check if the XMLHttpRequest object has a "withCredentials" property.
-      // "withCredentials" only exists on XMLHTTPRequest2 objects.
-      xhr.open(method, url, true);
-    } else {
-      // Otherwise, CORS is not supported by the browser.
-      xhr = null;
-    }
-    return xhr;
   }
 
   render() {
