3b871b462a882de31004a4203babe4c6e96a056d
Xavier Olivares <xm.olivares@gmail.com>
2019-07-02 16:08:45 -0400

    CHORE(jobBoard): rendering two scrolling columns in jobBoard component

--- src/component/JobBoard.js
+++ src/component/JobBoard.js
@@ -6,7 +6,7 @@ export default class JobBoard extends Component {
   constructor({ handleClose, show, children }) {
     super();
   }
-  
+
   render() {
     const showHideClassName = this.show
       ? "modal display-block"
@@ -14,12 +14,25 @@ export default class JobBoard extends Component {
     return (
       <div className="job-board">
         <h1>Jobs Near You: </h1>
-        <hr/>
-        <section>
-        {jobs.map(job => {
-          return <h1>{job.title}</h1>;
-        })}
-        </section>
+        <hr />
+        <div className="job-list-and-description">
+          <div className="job-board-info">
+            {jobs.map(job => {
+              return (
+                <div>
+                  <h1>{job.title}</h1>
+                  <hr/>
+                </div>
+              );
+            })}
+          </div>
+          <hr />
+          <div className="job-description">
+            {jobs.map(job => {
+              return <h1>{job.description}</h1>;
+            })}
+          </div>
+        </div>
       </div>
     );
   }
