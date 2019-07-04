8229d988bedcfa97c10abf52ccf3a749a34ba2c8
Xavier Olivares <xm.olivares@gmail.com>
2019-07-04 14:41:05 -0400

    fixed script and will test

---
+++ script.js
@@ -0,0 +1,29 @@
+const data = []
+
+const yearAverage = months => {
+  let newObj = {};
+  months.forEach(month => {
+    let temp = month["TAVG"];
+    const year = String(month.YearMonth).slice(0, 4);
+    if (typeof temp !== "number") {
+      // console.log("WHAT IS THIS", temp);
+      if (temp.includes("-.")) {
+        temp = temp.split("")
+        temp.shift()
+        temp.unshift("0");
+        temp = temp.join("")
+        temp = temp * -1;
+        console.log("this is temp", temp, 'typeof', typeof temp);
+        // console.log("TRANSFORMED", temp, "TYPEOF", typeof temp);
+      }
+    }
+    if (!newObj[year]) newObj[year] = temp;
+    else newObj[year] = newObj[year] + temp;
+  });
+  for (let year in newObj) {
+    newObj[year] = (newObj[year] / 12).toFixed(2);
+  }
+  return newObj;
+};
+
+console.log(yearAverage(data));
