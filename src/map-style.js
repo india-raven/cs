d1a9c9d82ceeb595a517fdb32998f2943d4a8ed6
Xavier Olivares <xm.olivares@gmail.com>
2019-07-04 18:06:49 -0400

    FIX(colorPalette): added more colors, 1911 and other years are problematic

--- src/map-style.js
+++ src/map-style.js
@@ -11,16 +11,39 @@ export const dataLayer = fromJS({
     "fill-color": {
       property: "percentile",
       stops: [
-        [0, "blue"], // dark blue
-        [1, "royalblue"],
-        [2, "cyan"],
-        [3, "green"],
-        [4, "lime"],
-        [5, "yellow"],
-        [6, "orange"],
-        [7, "pink"],
-        [8, "red"] // red
+        [0, "#0064FF"], 
+        [1, "#0094FF"],
+        [2, "#00C4FF"],
+        [3, "#00E4FF"],
+        [4, "#00FFD0"],
+        [5, "#00FF83"],
+        [6, "#00FF10"],
+        [7, "#65FF00"],
+        [8, "#B0FF00"],
+        [9, "#FDFF00"], 
+        [10, "#FFF000"],
+        [11, "#FFDC00"],
+        [12, "#FFC800"],
+        [13, "#FFB400"],
+        [14, "#FFA000"],
+        [15, "#FF8C00"],
+        [16, "#FF7800"],
+        [17, "#FF6400"],
+        [18, "#FF5000"],
+        [19, "#FF3C00"],
+        [20, "#FF2800"] 
       ]
+      // [
+      //   [0, "blue"], // dark blue
+      //   [1, "royalblue"],
+      //   [2, "cyan"],
+      //   [3, "green"],
+      //   [4, "lime"],
+      //   [5, "yellow"],
+      //   [6, "orange"],
+      //   [7, "pink"],
+      //   [8, "red"] // red
+      // ]
       // stops: [
       //   [0, "rgb(49,49,100)"], // dark blue
       //   [1, "rgb(40,40,82)"],
