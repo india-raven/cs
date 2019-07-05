96e11fb4c2df921895d6680a0c39f49a4169be1c
zyabb <48525052+zyabb@users.noreply.github.com>
2019-07-05 11:39:20 -0400

    Revert "Charts"

--- src/component/StateInfo.js
+++ src/component/StateInfo.js
@@ -1,61 +1,29 @@
-import React, { PureComponent } from "react";
-import ChartApp from "./charts/ChartApp";
-import Chart from "./charts/Chart";
-import ChartLine from "./charts/ChartLine";
-import ChartBubble from "./charts/ChartBubble";
+import React, { PureComponent } from 'react';
+
 //modal wrapper for our popup
 const defaultContainer = ({ children, classInStateInfo }) => (
   <div className={classInStateInfo}>{children}</div> //change
 );
 
 export default class StateInfo extends PureComponent {
-  constructor() {
-    super();
-    this.state = {
-      barChart: <Chart />, //barchart 
-      // donutChart: <ChartLine />, //donut
-      bubbleChart: <ChartBubble />, //
-      currentChart: <Chart />
-    };
-    this.changeChart = this.changeChart.bind(this);
-  }
-  changeChart() {
-    this.setState({
-      currentChart: <ChartBubble />
-    });
-  }
   render() {
     const { show, name, onClick } = this.props;
     const showHideClassName = show
-      ? "state-info display-block"
-      : "state-info display-none";
+      ? 'state-info display-block'
+      : 'state-info display-none';
 
     const Container = this.props.containerComponent || defaultContainer;
 
     return (
       <Container classInStateInfo={showHideClassName}>
-        {/* <div style={{position:'absolute', width:'10%'}}> */}
         <i className="fas fa-times fa-2x" onClick={onClick} />
         <h3>STATE {this.props.name}</h3>
         <h3>STATE DESCRIPTION</h3>
-        {/* <button type="button" onClick={this.changeChart}>
-          Change Chart1
-        </button> */}
-        {/* <div style={{ position: "relative", width: "10%" }}>
-          <div style={{ position: "absolute", width: "10%" }}>
-            {this.state.currentChart}
-          </div>
-        </div> */}
-        <Chart />
-        {/* <ChartBubble /> */}
-        {/* <p>chart 1 </p>
+        <p>chart 1</p>
         <p>chart 2</p>
         <p>chart 3</p>
         <p>chart 4</p>
-
-        <p>chart 5</p> */}
-        {/* <ChartApp className={showHideClassName}/> */}
-        {/* </div> */}
+        <p>chart 5</p>
       </Container>
     );
   }
