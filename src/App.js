87cb9b8ba7f5644d59cec5b1063cf1931211e25f
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-07-05 16:09:18 -0400

    FEAT(stripe): added basic stripe component with keys

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import './App.css';
-import Map from './Map';
-import Fire from './Fire';
+import React from "react";
+import "./App.css";
+import Map from "./Map";
+import Fire from "./Fire";
+import Checkout from "./component/Checkout";
 
 function App() {
   return (
     <div className="App">
-      <Map />
+      {/* <Map /> */}
+      <Checkout
+        name={"India Raven"}
+        description={"Fight climate change"}
+        amount={1}
+      />
     </div>
   );
 }
