e3763df83b869494f8025c0e2bec4ad2d3d3d9ad
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-07-05 20:08:23 -0400

    FEAT(pdsi values): added pdsi values to all states

--- src/component/stripe/constants/stripe.js
+++ src/component/stripe/constants/stripe.js
@@ -1,5 +1,5 @@
 const STRIPE_PUBLISHABLE =
-  process.env.NODE_ENV === process.env.STRIPE_PUBLISHABLE_Key;
+  process.env.NODE_ENV === process.env.STRIPE_PUBLISHABLE_KEY;
 
 // "production"
 //   ? "pk_live_MY_PUBLISHABLE_KEY"
