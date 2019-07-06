ff3075c22f341a649a03da09be4a9197d85385f7
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-07-06 13:48:15 -0400

    FIX(slider): added more ticks

--- src/control-panel.js
+++ src/control-panel.js
@@ -50,7 +50,7 @@ export default class ControlPanel extends PureComponent {
                 className="slider"
                 type="range"
                 value={settings.year}
-                min={1895}
+                min={1925}
                 max={2018}
                 step={1}
                 onChange={evt => this.props.onChange("year", evt.target.value)}
@@ -58,8 +58,18 @@ export default class ControlPanel extends PureComponent {
             </div>
             <div className="sliderticks">
               <p>1925</p>
+              <p>1934</p>
+              <p>1945</p>
+              <p>1955</p>
+              <p>1962</p>
               <p>1971</p>
-              <p>2018</p>
+              <p>1980</p>
+              <p>1990</p>
+              <p>2005</p>
+              <p>2008</p>
+              <div className="tick-2018">
+                <p>2018</p>
+              </div>
             </div>
           </div>
         </div>
