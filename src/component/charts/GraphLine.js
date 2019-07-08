ecb7ef49df485248ed33d0cd6381cfa48178e31c
Ruslan Zyabbarov <rupam71@gmail.com>
2019-07-08 11:37:02 -0400

    GraphLine commented out for correct render. Package-lock.json updated

--- src/component/charts/GraphLine.js
+++ src/component/charts/GraphLine.js
@@ -18,59 +18,56 @@ class GraphLine extends Component {
     this.yScale = scaleLinear();
   }
 
-  convertObjToArr = obj => {
-    if (obj) {
-      let objKeys = Object.keys(obj);
-      return objKeys.map(key => {
-        let newObj = {};
-        newObj.date = key;
-        newObj.y = obj[key];
-        return newObj;
-      });
-    }
-  };
+  // convertObjToArr = obj => {
+  //   if (obj) {
+  //     let objKeys = Object.keys(obj);
+  //     return objKeys.map(key => {
+  //       let newObj = {};
+  //       newObj.date = key;
+  //       newObj.y = obj[key];
+  //       return newObj;
+  //     });
+  //   }
+  // };
 
   render() {
-    let temp = this.props.stateData;
+    // let temp = this.props.stateData;
 
-    if (temp.temperature) {
-      temp = JSON.parse(temp.temperature);
-      const data = this.convertObjToArr(temp);
-      console.log('=======>', data);
-      let margins = { top: 20, right: 30, bottom: 30, left: 40 };
-      // return new Date(parseInt(d.Year),0)
-      const svgDimensions = { width: 500, height: 400 };
-      const max = d3.max(data, d => Math.abs(d.y));
-      const min = d3.min(data, d => Math.abs(d.y));
-      console.log(d3.extent(data, d => d.date));
-      const xScale = this.xScale
-        .domain(d3.extent(data, d => d.date))
-        .range([margins.left, svgDimensions.width - margins.right]);
-      const yScale = this.yScale
-        .domain([0, d3.max(data, d => d.y)])
-        .nice()
-        .range([svgDimensions.height - margins.bottom, margins.top]);
+    // temp = JSON.parse(temp.temperature);
+    // const data = this.convertObjToArr(temp);
+    // console.log('=======>', data);
+    // let margins = { top: 20, right: 30, bottom: 30, left: 40 };
+    // // return new Date(parseInt(d.Year),0)
+    // const svgDimensions = { width: 500, height: 400 };
+    // const max = d3.max(data, d => Math.abs(d.y));
+    // const min = d3.min(data, d => Math.abs(d.y));
+    // console.log(d3.extent(data, d => d.date));
+    // const xScale = this.xScale
+    //   .domain(d3.extent(data, d => d.date))
+    //   .range([margins.left, svgDimensions.width - margins.right]);
+    // const yScale = this.yScale
+    //   .domain([0, d3.max(data, d => d.y)])
+    //   .nice()
+    //   .range([svgDimensions.height - margins.bottom, margins.top]);
 
-      return (
-        <svg width={svgDimensions.width} height={svgDimensions.height}>
-          <AxesL
-            scales={{ xScale, yScale }}
-            margins={margins}
-            svgDimensions={svgDimensions}
-          />
-          <Line
-            scales={{ xScale, yScale }}
-            svgDimensions={svgDimensions}
-            margins={margins}
-            maxValue={max}
-            minValue={min}
-            data={data}
-          />
-        </svg>
-      );
-    } else {
-      return <div />;
-    }
+    return (
+      // <svg width={svgDimensions.width} height={svgDimensions.height}>
+      //   <AxesL
+      //     scales={{ xScale, yScale }}
+      //     margins={margins}
+      //     svgDimensions={svgDimensions}
+      //   />
+      //   <Line
+      //     scales={{ xScale, yScale }}
+      //     svgDimensions={svgDimensions}
+      //     margins={margins}
+      //     maxValue={max}
+      //     minValue={min}
+      //     data={data}
+      //   />
+      // </svg>
+      <h1>hello</h1>
+    );
   }
 }
 export default GraphLine;
