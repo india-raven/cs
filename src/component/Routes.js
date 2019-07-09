4178314ffc46c30cb80103eb6d4e00df0230aa7e
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-07-09 14:08:42 -0400

    FIX(router): national stats button routes to appropriate component

--- src/component/Routes.js
+++ src/component/Routes.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
 import { withRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import Map from "../Map";
 import NewPage from "../component/totalInfo/newPage";
 
-class Routes extends Component {
-  componentDidMount() {
-    this.props.loadInitialData();
-  }
+export default class Routes extends Component {
+  // componentDidMount() {
+  //   this.props.loadInitialData();
+  // }
 
   render() {
     const { isLoggedIn } = this.props;
