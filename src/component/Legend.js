9c1b262d093dda104f7cb3de7501f62957ff0dbe
Xavier Olivares <xm.olivares@gmail.com>
2019-07-08 20:24:14 -0400

    FIX(legendComponent): created a trigger component to switch legends based on selected map type

--- src/component/Legend.js
+++ src/component/Legend.js
@@ -2,7 +2,10 @@ import React, {Component} from 'react'
 
 export default class Legend extends Component {
     render() {
+    const selectedData= this.props.selectedData
+    console.log(selectedData)
         return (
+            selectedData === 'Temperature' ?
 <div id='state-legend' class='legend'>
 <div><h3>Hot</h3></div>
 <div><span style={{background: '#723122'}}></span></div>
@@ -15,6 +18,18 @@ export default class Legend extends Component {
 <div><span style={{background: '#EED322'}}></span></div>
 <div><span style={{background: '#F2F12D'}}></span></div>
 <div><h3>Cold</h3></div>
+</div> : <div id='state-legend' class='legend'>
+<div><h3>High</h3></div>
+<div><span style={{background: '#723122'}}></span></div>
+<div><span style={{background: '#8B4225'}}></span></div>
+<div><span style={{background: '#A25626'}}></span></div>
+<div><span style={{background: '#B86B25'}}></span></div>
+<div><span style={{background: '#CA8323'}}></span></div>
+<div><span style={{background: '#DA9C20'}}></span></div>
+<div><span style={{background: '#E6B71E'}}></span></div>
+<div><span style={{background: '#EED322'}}></span></div>
+<div><span style={{background: '#F2F12D'}}></span></div>
+<div><h3>Low</h3></div>
 </div>
         )
     }
