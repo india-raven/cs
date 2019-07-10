67518a61f019a8ca2cf86ead7cd0d3094c05c7ba
Ruslan Zyabbarov <rupam71@gmail.com>
2019-07-10 17:36:22 -0400

    extra minor changes

--- src/component/JobDescription.js
+++ src/component/JobDescription.js
@@ -1,21 +1,24 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 export const JobDescription = props => {
   return (
     <div className="job-exact">
       <h1>Job title: {props.job.title}</h1>
       <h2>
-        <i className="far fa-building" /> {props.job.company}
+        {/* <i className="far fa-building" />  */}
+        {props.job.company}
       </h2>
       {/* <h2>Location: {props.job.location}</h2> */}
       <h2>
-        <i className="fas fa-map-marked-alt" /> {props.job.location}
+        {/* <i className="fas fa-map-marked-alt" /> */}
+        {props.job.location}
       </h2>
-      <a target={"_blank"} href={props.job.url}>
+      <a target={'_blank'} href={props.job.url}>
         Get more info
       </a>
       <h2>
-        <i className="fas fa-calendar-check" /> {props.job.created_at}
+        {/* <i className="fas fa-calendar-check" />  */}
+        {props.job.created_at}
       </h2>
       <p>Description: {props.job.description}</p>
     </div>
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,7 +1,7 @@
-import React, { PureComponent } from "react";
-import { Link } from "react-router-dom";
-import { Button } from "@material-ui/core";
-import { json as requestJson } from "d3-request";
+import React, { PureComponent } from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from '@material-ui/core';
+import { json as requestJson } from 'd3-request';
 
 const defaultContainer = ({ children }) => (
   <div className="navbar">{children}</div>
@@ -15,34 +15,38 @@ export default class ControlPanel extends PureComponent {
   render() {
     const Container = this.props.containerComponent || defaultContainer;
     const { settings } = this.props;
-    const logo = require("../VisualClimate_logo.png");
+    const logo = require('../VisualClimate_logo.png');
 
     return (
       <Container>
-        <div style={{ display: "flex" }}>
+        <div style={{ display: 'flex' }}>
           <div
             style={{
-              display: "flex",
-              justifyContent: "center",
+              display: 'flex',
+              justifyContent: 'center',
               flex: 2,
-              width: "auto"
+              width: 'auto',
             }}
           >
             <Button component={AdapterLink} to="/">
               Home
             </Button>
-            <Button component={AdapterLink} to="/usa">
+            {/* <Button component={AdapterLink} to="/usa">
               National Stats
+            </Button> */}
+            <Button component={AdapterLink} to="/blog">
+              Blog
             </Button>
-                <Button component={AdapterLink} to='/blog'>Blog</Button>
-            <Button component={AdapterLink} to="/about">About</Button>
+            <Button component={AdapterLink} to="/about">
+              About
+            </Button>
           </div>
-          <hr style={{ margin: "1px" }} />
-          <div style={{ display: "flex", justifyContent: "center", flex: 1 }}>
+          <hr style={{ margin: '1px' }} />
+          <div style={{ display: 'flex', justifyContent: 'center', flex: 1 }}>
             <img
               src={logo}
               alt="VisualClimate logo"
-              style={{ maxWidth: "80%" }}
+              style={{ maxWidth: '80%' }}
             />
           </div>
         </div>
