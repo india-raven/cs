d60677de2d827f6c1a48e81edf28648a8fe74eb4
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-07-10 11:17:31 -0400

    FIX(sense): removed

--- src/GeoFire.js
+++ src/GeoFire.js
@@ -9,13 +9,13 @@ class GeoFire {
   init = () => {
     if (!firebase.apps.length) {
       firebase.initializeApp({
-        apiKey: "AIzaSyC5grXJ2ztg8V_6OpintdIY1j8jYE1-IL0",
-        authDomain: process.env.GEOFIRE_APP_AUTH_DOMAIN,
-        databaseURL: "https://india-raven-geojson.firebaseio.com",
-        projectId: process.env.GEOFIRE_APP_PROJECT_ID,
-        storageBucket: process.env.GEOFIRE_APP_STORAGE_BUCKET,
-        messagingSenderId: process.env.GEOFIRE_APP_MESSAGING_SENDER_ID,
-        appId: process.env.GEOFIRE_APP_APP_ID
+        apiKey: process.env.REACT_APP_API_KEY,
+        authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+        databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+        projectId: process.env.REACT_APP_PROJECT_ID,
+        storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+        messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+        appId: process.env.REACT_APP_APP_ID
       });
     }
   };
