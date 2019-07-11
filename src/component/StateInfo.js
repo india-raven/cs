c36f8cff6e078e931270920b57e0826838130f0c
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-07-11 17:44:55 -0400

    added jobs, changed state info title area

--- src/component/StateInfo.js
+++ src/component/StateInfo.js
@@ -1,13 +1,13 @@
-import React, { PureComponent } from 'react';
-import { Button } from '@material-ui/core';
+import React, { PureComponent } from "react";
+import { Button } from "@material-ui/core";
 
-import ChartApp from './charts/ChartApp';
-import Chart from './charts/Chart';
-import ChartLine from './charts/ChartLine';
-import ChartBubble from './charts/ChartBubble';
-import LineChartSideBar from './charts/LineChartSideBar';
-import LinePDChart from './charts/LinePDChart';
-import JobBoard from './JobBoard';
+import ChartApp from "./charts/ChartApp";
+import Chart from "./charts/Chart";
+import ChartLine from "./charts/ChartLine";
+import ChartBubble from "./charts/ChartBubble";
+import LineChartSideBar from "./charts/LineChartSideBar";
+import LinePDChart from "./charts/LinePDChart";
+import JobBoard from "./JobBoard";
 //modal wrapper for our popup
 const defaultContainer = ({ children, classInStateInfo }) => (
   <div className={classInStateInfo}>{children}</div> //change
@@ -17,7 +17,7 @@ export default class StateInfo extends PureComponent {
   constructor() {
     super();
     this.state = {
-      show: false,
+      show: false
     };
   }
 
@@ -32,8 +32,8 @@ export default class StateInfo extends PureComponent {
   render() {
     const { show, name, onClick, stateData } = this.props;
     const showHideClassName = show
-      ? 'state-info display-block'
-      : 'state-info display-none';
+      ? "state-info display-block"
+      : "state-info display-none";
 
     const Container = this.props.containerComponent || defaultContainer;
 
@@ -42,19 +42,19 @@ export default class StateInfo extends PureComponent {
         {/* <div style={{position:'absolute', width:'10%'}}> */}
         <i className="fas fa-times fa-2x" onClick={onClick} />
         <div className="state-info-title-and-btn">
-          <h3>{name}</h3>
+          <h2>{name}</h2>
           <div id="jobs-btn">
             <Button type="button" variant="contained" onClick={this.showModal}>
-              Climate-related job opportunities
+              See climate jobs
             </Button>
           </div>
           <JobBoard show={this.state.show} handleClose={this.hideModal} />
         </div>
         <hr />
-        <div>{'Average Temperature \u2109 (1895-2018)'}</div>
+        <div>{"Average Temperature \u2109 (1895-2018)"}</div>
         <LineChartSideBar stateData={stateData.temperature} />
         <hr />
-        <div>{'Average PDSI (1925-2018)'}</div>
+        <div>{"Average PDSI (1925-2018)"}</div>
         <LinePDChart stateData={stateData.pdsi} />
       </Container>
     );
