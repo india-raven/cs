548b3e35cc5a83042d09bc65a4430f70d2ae24c4
Alyona <alyona.rodin@gmail.com>
2019-07-29 14:57:02 -0400

    feat: added axis names on the line chart

--- src/component/charts/AxesL.js
+++ src/component/charts/AxesL.js
@@ -24,6 +24,27 @@ export default ({ scales, margins, svgDimensions }) => {
       <g>
         <Axis {...xProps} />
         <Axis {...yProps} />
+        <text
+        y='6'
+        dy='8em'
+        transform ="rotate(-90)"
+        fill="black"
+        text-anchor="end"
+        font-size="10px"
+        >
+          {'parts-per-million (ppm)'}
+        </text>
+        <text
+          y='4'
+          dy="39em"
+          dx='77em'
+          transform ="rotate(0)"
+          fill="black"
+          text-anchor="start"
+          font-size="10px"
+        >
+          {'Year (1960-2015)'}
+        </text>
       </g>
     )
   }
