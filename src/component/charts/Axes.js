d0de7cb02ab81553b1504808cf89772bab649e6f
Alyona <alyona.rodin@gmail.com>
2019-07-29 14:54:42 -0400

    feat: added axis names

--- src/component/charts/Axes.js
+++ src/component/charts/Axes.js
@@ -16,12 +16,33 @@ export default ({ scales, margins, svgDimensions }) => {
     scale: scales.yScale,
     translate: `translate(${margins.left}, 0)`,
     tickSize: width - margins.left - margins.right,
+    
   };
-  // console.log(scales)
   return (
     <g>
       <Axis {...xProps} />
       <Axis {...yProps} />
+      <text
+          y='25'
+          dy="0.5em"
+          transform ="rotate(-90)"
+          fill="black"
+          text-anchor="end"
+          font-size="10px"
+        >
+          {'Temperature \u2103'}
+        </text>
+        <text
+          y='4'
+          dy="29em"
+          dx='2em'
+          transform ="rotate(0)"
+          fill="black"
+          text-anchor="start"
+          font-size="10px"
+        >
+          {'Year/month (2010-2019)'}
+        </text>
     </g>
   );
 };
